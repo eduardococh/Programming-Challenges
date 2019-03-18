919f5297159db8bd3f2ede34324ef7fc9fde15d3
Eduardo Contreras <eduardoverd_@hotmail.com>
2019-03-18 15:34:35 -0600

    Add files via upload
    104 Maximum Depth Binary Tree

---
+++ 104MaximumDepthBinaryTree.java
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        int max=1,current=1;
+        if(root!=null){
+            return exploreTree(root,current,max);   
+        }else{
+            return 0;
+        }
+    }
+    
+    public int exploreTree(TreeNode node,int current,int max){
+        if(current>max){
+            max=current;
+        }
+        current++;
+        if(node.left!=null){
+            max=exploreTree(node.left,current,max);
+        }
+        if(node.right!=null){
+            max=exploreTree(node.right,current,max);
+        }
+        return max;
+    }
+
+    //Simple
+    public int maxDepth(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        return 1+Math.max(maxDepth(root.left),maxDepth(root.right));
+    }
+}
