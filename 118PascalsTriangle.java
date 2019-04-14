1ef0c7ed5138874cf3331dcc048685be7eddfdd3
Eduardo Contreras <eduardoverd_@hotmail.com>
2019-04-13 20:37:22 -0500

    https://leetcode.com/problems/pascals-triangle/
    Updated more solutions

--- 118PascalsTriangle.java
+++ 118PascalsTriangle.java
@@ -1,4 +1,9 @@
 class Solution {
+      //My solution, faster and better memory than 100% (weirdly same algorithm took different time in two submissions)
+      //https://leetcode.com/submissions/detail/222232644/
+      //https://leetcode.com/submissions/detail/221991646/
+      //Runtime o(n2)
+      //This was a dynamic programming solution, even thou i didnt know it until i saw the article, where it is the only solution
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> result=new ArrayList<List<Integer>>();
         if(numRows==0){
@@ -24,4 +29,23 @@ class Solution {
         }
         return result;
     }
+}
+
+//Concise solution by leetcode rheaxu
+//clever handling of corner cases for 0 and 1 and just more elegant, same runtime and memory
+public class Solution {
+   public List<List<Integer>> generate(int numRows){
+   	List<List<Integer>> allrows = new ArrayList<List<Integer>>();
+   	ArrayList<Integer> row = new ArrayList<Integer>();
+      
+   	for(int i=0;i<numRows;i++)
+   	{
+   		row.add(0, 1);
+   		for(int j=1;j<row.size()-1;j++)
+   			row.set(j, row.get(j)+row.get(j+1));
+   		allrows.add(new ArrayList<Integer>(row));
+   	}
+   	return allrows;
+   	
+   }
 }
