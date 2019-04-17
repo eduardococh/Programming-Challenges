9a008b2488d73ec094e4bd7e1be77005d50af229
Eduardo Contreras <eduardoverd_@hotmail.com>
2019-04-16 23:30:29 -0500

    https://leetcode.com/problems/subarray-sum-equals-k/

--- 560SubarraySumEqualsK.java
+++ 560SubarraySumEqualsK.java
@@ -1,5 +1,6 @@
 public class Solution {
-		//Brute force n(n3) time limit exceded
+		//Brute force o(n3) time limit exceded and o(1) memory
+		//"Easy" approach but not optimal at all
     public int subarraySum(int[] nums, int k) {
         int count = 0;
         for (int start = 0; start < nums.length; start++) {
@@ -16,6 +17,8 @@ public class Solution {
 }
 
 public class Solution {
+		//The key to this one is keeping a cumulative sum, so we dont have to calculate sum for every
+		//Subarray, bringing the complexity to o(n2), memory of o(n) since we store one array n
     public int subarraySum(int[] nums, int k) {
         int count = 0;
         int[] sum = new int[nums.length + 1];
@@ -27,6 +30,42 @@ public class Solution {
                 if (sum[end] - sum[start] == k)
                     count++;
             }
+        }
+        return count;
+    }
+}
+
+public class Solution {
+		//The key here is to avoid using a subsum array with a one pass solution, really simple
+		//Checks every subarray in a pattern like inverted pyramid, o(n2) runtime and o(1) memory
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+        for (int start = 0; start < nums.length; start++) {
+            int sum=0;
+            for (int end = start; end < nums.length; end++) {
+                sum+=nums[end];
+                if (sum == k)
+                    count++;
+            }
+        }
+        return count;
+    }
+}
+
+public class Solution {
+		//Best solution, understanging this is key, o(n) runtime and o(n) memory
+		//So we store sum in a hashmap, and whenever we repeat a sum we add it to the count
+		//We put the first   map.put(0, 1); so we can consider the number k itself
+		
+    public int subarraySum(int[] nums, int k) {
+        int count = 0, sum = 0;
+        HashMap < Integer, Integer > map = new HashMap < > ();
+        map.put(0, 1);
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (map.containsKey(sum - k))
+                count += map.get(sum - k);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
         return count;
     }
