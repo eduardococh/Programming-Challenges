4908e29302905a7cf206211e8257c633fa1e5d27
Eduardo Contreras <eduardoverd_@hotmail.com>
2019-05-02 23:08:37 -0500

    https://leetcode.com/problems/remove-element/

--- 27RemoveElement.java
+++ 27RemoveElement.java
@@ -25,3 +25,29 @@ class Solution {
         return res;
     }
 }
+
+public int removeElement(int[] nums, int val) {
+    int i = 0;
+    for (int j = 0; j < nums.length; j++) {
+        if (nums[j] != val) {
+            nums[i] = nums[j];
+            i++;
+        }
+    }
+    return i;
+}
+
+public int removeElement(int[] nums, int val) {
+    int i = 0;
+    int n = nums.length;
+    while (i < n) {
+        if (nums[i] == val) {
+            nums[i] = nums[n - 1];
+            // reduce array size by one
+            n--;
+        } else {
+            i++;
+        }
+    }
+    return n;
+}
