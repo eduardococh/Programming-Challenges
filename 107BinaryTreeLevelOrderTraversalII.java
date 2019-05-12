86d5deb9c31586272d15a20f59c429f0ab404836
Eduardo Contreras <eduardoverd_@hotmail.com>
2019-05-12 12:49:16 -0500

    https://leetcode.com/problems/binary-tree-level-order-traversal-ii/

--- 107BinaryTreeLevelOrderTraversalII.java
+++ 107BinaryTreeLevelOrderTraversalII.java
@@ -28,3 +28,67 @@ class Solution {
         return result;
     }
 }
+
+
+		//My solution using queue, a more natural approach to this problem
+		//Runtime of 1ms, better than 88.59%
+		//Memory of 35.4 less than 80.5%
+		//Good practice of a basic algorithm
+class Solution {
+    
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result=new ArrayList<List<Integer>>();
+        Queue<TreeNode> treeQ=new LinkedList<TreeNode>();
+        if(root!=null){
+            treeQ.add(root);
+        }
+        while(!treeQ.isEmpty()){
+            int levelLength=treeQ.size();
+            List<Integer> thisLevel=new ArrayList<Integer>();
+            for(int i=0;i<levelLength;i++){
+                TreeNode current=treeQ.poll();
+                thisLevel.add(current.val);
+                if(current.left!=null){
+                    treeQ.add(current.left);
+                }
+                if(current.right!=null){
+                    treeQ.add(current.right);   
+                }
+            }
+            result.add(0,thisLevel);
+            
+        }
+        return result;
+    }
+    
+}
+
+
+		//Leetcode's SOY DFS approach
+		//Same runtime and memory (small .1mb memory improvement over BFS)
+		//Improvement over my ugly recursive solution
+		//interesting the way he doesn't return anything in their method, only
+		//alters object
+
+public class Solution {
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> wrapList = new LinkedList<List<Integer>>();
+        levelMaker(wrapList, root, 0);
+        return wrapList;
+    }
+
+    public void levelMaker(List<List<Integer>> list, TreeNode root, int level) {
+
+        if(root == null) return;
+
+        if(level >= list.size()) {
+            list.add(0, new LinkedList<Integer>());
+        }
+
+        levelMaker(list, root.left, level+1);
+        levelMaker(list, root.right, level+1);
+        list.get(list.size()-level-1).add(root.val);
+
+    }
+}
