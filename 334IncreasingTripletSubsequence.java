eb91d5586b01b1531e640ea91f4a7cc0a8fd6bee
Eduardo Contreras <eduardoverd_@hotmail.com>
2019-06-18 21:12:50 -0500

    https://leetcode.com/problems/longest-increasing-subsequence/

---
+++ 334IncreasingTripletSubsequence.java
@@ -0,0 +1,15 @@
+		//Solution by leetcode's sim5
+		//Really easy and elegant solution
+		//Amazing runtime of 0ms better than 100% O(n)
+		//Great memory of 38.8mb better than 95.70% o(1)
+  
+public boolean increasingTriplet(int[] nums) {
+        // start with two largest values, as soon as we find a number bigger than both, while both have been updated, return true.
+        int small = Integer.MAX_VALUE, big = Integer.MAX_VALUE;
+        for (int n : nums) {
+            if (n <= small) { small = n; } // update small if n is smaller than both
+            else if (n <= big) { big = n; } // update big only if greater than small but smaller than big
+            else return true; // return if you find a number bigger than both
+        }
+        return false;
+    }
