23f0de7c747a301e4353632796842b8b270c4711
Eduardo Contreras <eduardoverd_@hotmail.com>
2019-06-23 23:37:09 -0500

    https://leetcode.com/problems/range-sum-query-immutable/

--- 303RangeSumQuery-Inmutable.java
+++ 303RangeSumQuery-Inmutable.java
@@ -14,3 +14,20 @@ class NumArray {
         return res;
     }
 }
+
+class NumArray {
+
+    private int[] data;
+
+    public NumArray(int[] nums) {
+        data = nums;
+    }
+
+    public int sumRange(int i, int j) {
+        int sum = 0;
+        for (int k = i; k <= j; k++) {
+            sum += data[k];
+        }
+        return sum;
+    }
+}
