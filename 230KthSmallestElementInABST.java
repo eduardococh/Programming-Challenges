15d898826f49c54af709b654b0794b381717efa8
Eduardo Contreras <eduardoverd_@hotmail.com>
2019-06-26 23:20:53 -0500

    https://leetcode.com/problems/kth-smallest-element-in-a-bst/

--- 230KthSmallestElementInABST.java
+++ 230KthSmallestElementInABST.java
@@ -12,3 +12,42 @@ class Solution {
     return nums.get(k - 1);
   }
 }
+
+
+		//My complex iterative solution (my recursive solution with same approach
+		//was equally bad)
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        boolean left=false;
+        if(root==null) return 0;
+        Stack<TreeNode> st=new Stack();
+        if(root.left==null){
+            st.push(root);
+        }
+        while(root.left!=null){
+            left=true;
+            st.push(root);
+            TreeNode father=root;
+            root=root.left;
+            father.left=null;
+        }
+        k--;
+        TreeNode current=root;
+        while(k>0 && !st.isEmpty()){
+            //System.out.println("we addd");
+            current=st.pop();
+            k--;
+            if(current.left!=null){
+                left=true;
+                st.push(current.left);
+            }
+            if(current.right!=null){
+                st.push(current.right);
+            }
+        }
+        if(!left){
+            current=st.pop();
+        }
+        return current.val;
+    }
+}
