0a8e2f6690ebb2592fa62124f7207564f38cac70
Eduardo Contreras <eduardoverd_@hotmail.com>
2019-06-30 23:18:23 -0500

    https://leetcode.com/problems/valid-perfect-square/

---
+++ 367ValidPerfectSquare.java
@@ -0,0 +1,11 @@
+		//from leetcode's fhqplzj
+class Solution {
+    public boolean isPerfectSquare(int num) {
+     int i = 1;
+     while (num > 0) {
+         num -= i;
+         i += 2;
+     }
+     return num == 0;
+ }
+}
