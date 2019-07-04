d592f47b1038a103ce4057e2d3ba2720b1b9c020
Eduardo Contreras <eduardoverd_@hotmail.com>
2019-07-03 23:27:44 -0500

    https://leetcode.com/problems/ransom-note/

--- 383RansomNote.java
+++ 383RansomNote.java
@@ -1,3 +1,7 @@
+		//My solution
+		//Simple and direct
+		//Good runtime better than 75.94% O(n+m)
+		//Amazing memory better than 99.15% O(1)
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
         if(ransomNote.length()>magazine.length()){
@@ -12,6 +16,26 @@ class Solution {
             if(letters[ransomNote.charAt(i)-97]<0){
                 return false;
             }
+        }
+        return true;
+    }
+}
+
+
+		//Good simple improvement, use .toCharArray(), avoid multiple charAt's
+		//Great runtime of 2ms better than 98.62%
+		//Amazing memory better than 99.13% 
+		//0ms solution is not so trivial so not going to cover it
+
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] list = new int[26];
+        for(char c:magazine.toCharArray()){
+            list[c-'a']++;
+        }
+        for(char c: ransomNote.toCharArray()){
+            list[c-'a']--;
+            if(list[c-'a']<0) return false;
         }
         return true;
     }
