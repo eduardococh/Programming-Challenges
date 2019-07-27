1e410fde2bd85c7bf9c67e0e0fbc68fa62d71a1c
Eduardo Contreras <eduardoverd_@hotmail.com>
2019-07-26 23:53:24 -0500

    https://leetcode.com/problems/island-perimeter/

---
+++ Arrays/463IslandPerimeter.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int islandPerimeter(int[][] grid) {
+        int perimeter=0;
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]==1){
+                    if(i==0 || grid[i-1][j]==0){
+                        perimeter++;
+                    }
+                    if(j==0 || grid[i][j-1]==0){
+                        perimeter++;
+                    }
+                    if(i==grid.length-1 || grid[i+1][j]==0){
+                        perimeter++;
+                    }
+                    if(j==grid[0].length-1 || grid[i][j+1]==0){
+                        perimeter++;
+                    }
+                }
+            }
+        }
+        return perimeter;
+    }
+}
