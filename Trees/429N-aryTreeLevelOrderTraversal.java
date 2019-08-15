8c16c1a11d3426c9b1c52de296b587b1808d1926
Eduardo Contreras <eduardoverd_@hotmail.com>
2019-08-14 20:16:31 -0500

    https://leetcode.com/problems/n-ary-tree-level-order-traversal/

--- Trees/429N-aryTreeLevelOrderTraversal.java
+++ Trees/429N-aryTreeLevelOrderTraversal.java
@@ -1,17 +1,8 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public List<Node> children;
-
-    public Node() {}
-
-    public Node(int _val,List<Node> _children) {
-        val = _val;
-        children = _children;
-    }
-};
-*/
+        //My solution
+        //Very simple and straightforward
+        //Bad runtime at 4ms worse than 10.87% O(N)
+        //Good memory 46.4mb better than 77.50% O(N)
+        //Could be improved
 class Solution {
     public List<List<Integer>> levelOrder(Node root) {
         List<List<Integer>> result=new ArrayList<List<Integer>>();
@@ -37,5 +28,69 @@ class Solution {
             }
         }
         return result;
+    }
+}
+
+        //Iterative solution
+        //Bit more elegant than mine, avoid a levelCounter
+class Solution {
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> list = new ArrayList();
+        
+        if(root==null) return list;
+        
+        Queue<Node> queue = new LinkedList();
+        
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            
+            List<Integer> subList = new ArrayList();
+            for(int i=0; i<size; i++){
+                Node curr = queue.poll();
+                subList.add(curr.val);
+                
+                for(Node item: curr.children){
+                    queue.offer(item);
+                }
+            }
+            list.add(subList);            
+        }
+        return list;
+        
+    }
+}
+
+        //Recursive solution from leetcode's 1ms sample
+        //Amazing runtime of 1ms better than 100%
+        //Bad memory 49.9mb better than 17.50%
+        //Interesting approach but bad memory
+class Solution {
+    public List<List<Integer>> levelOrder(Node root) {
+        
+        List<List<Integer>> mList = new ArrayList<>();
+        int firstLevel = 0;
+        
+        Recurse(root, firstLevel, mList);
+        
+        return mList;
+    }
+    
+    public void Recurse(Node n, int level, List<List<Integer>> mList) {
+        
+        if (n == null) {
+            return;
+        }
+        
+        if (mList.size() == level) {
+            mList.add(new ArrayList<>());
+        }
+        
+        mList.get(level).add(n.val);
+        
+        for (Node a : n.children) {
+            Recurse(a, level + 1, mList);
+        }        
+
     }
 }
