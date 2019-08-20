9bec57315f543453eb76033b3c288b4f9f521ce4
Eduardo Contreras <eduardoverd_@hotmail.com>
2019-08-19 21:03:15 -0500

    https://leetcode.com/problems/maximum-product-subarray/

--- Design/152MaximumProductSubarray.java
+++ Design/152MaximumProductSubarray.java
@@ -1,44 +1,56 @@
+        //Surprinsingly simple solution from leetcode's 0ms samples
+        //I spent two hours trying to do this simple thing
+        //The principle behind how simple it is is that when none numbers
+        //appear in pairs they can be multiplied to a positive, and when none numbers
+        //apear in none ocations the maximum can be calculated by right or left
+        //This is apparently faster than even the dp solution explored below
+        //Amazing runtime better than 100% O(N)
+        //Amazing memory better than 100% O(N)
 class Solution {
     public int maxProduct(int[] nums) {
-        if(nums==null || nums.length==0) return 0;
-        if(nums.length==1) return nums[0];
-        int currentLargest=nums[0],currentLargestNeg=nums[0],currentMul=nums[0];
-        for(int i=1;i<nums.length;i++){
-            //System.out.println(currentMul);
-            //System.out.println(currentLargestNeg);
-            
-                //0 breaks sequence positive and negative
-            if(nums[i]==0){
-                if(currentMul>currentLargest){
-                    currentLargest=currentMul;
-                }
-                currentMul=0;
-                currentLargestNeg=0;
-                
-                //Negative number can be added only on pairs
-            }else if(nums[i]<0){
-                currentLargestNeg*=nums[i];
-                currentMul=1;
-                
-                //Number is positive, we can extend currentMul
-            }else{
-                currentMul*=nums[i];
-                currentLargestNeg*=nums[i];
-            }
-            
-            if(currentMul>currentLargest){
-                currentLargest=currentMul;
-            }
-            if(currentLargestNeg>currentLargest){
-                currentLargest=currentLargestNeg;
-            }
-            
+        
+        int product = 1;
+        int max = Integer.MIN_VALUE;
+        
+        if(nums.length == 0 || nums == null) {
+            return 0;
         }
-        return currentLargest;
+        
+        //left to right
+        for(int i = 0; i < nums.length; i++) {
+            product *= nums[i];
+            if(product > max) {
+                max = product;
+            }
+            
+            if(product == 0) {
+                product = 1;
+            }
+        }
+        
+        //right to left
+        product = 1;
+        for(int i = nums.length - 1; i >= 0; i--) {
+            product *= nums[i]; 
+            if(product > max) {
+                 max = product; 
+            }
+            
+            if(product == 0) {
+                product = 1;
+            }
+        
+        }
+        return max; 
+        
     }
 }
 
-
+        //Solution by leetcode's mzchen 
+        //Dynamic programming approach
+        //It self explains with commentaries provided  by mzchen
+        //Amazing runtime better than 99%
+        //Amazing memory better than 100%
 int maxProduct(int A[], int n) {
     // store the result that is the max we have found so far
     int r = A[0];
@@ -46,7 +58,8 @@ int maxProduct(int A[], int n) {
     // imax/imin stores the max/min product of
     // subarray that ends with the current number A[i]
     for (int i = 1, imax = r, imin = r; i < n; i++) {
-        // multiplied by a negative makes big number smaller, small number bigger
+        // multiplied by a negative makes big number smaller (10*-2=-20),
+        // small number bigger                               (5*-2=-10)
         // so we redefine the extremums by swapping them
         if (A[i] < 0)
             swap(imax, imin);
@@ -60,4 +73,51 @@ int maxProduct(int A[], int n) {
         r = max(r, imax);
     }
     return r;
+}
+/* Working java solution from approach above
+class Solution {
+    public int maxProduct(int[] nums) {
+        int max = Integer.MIN_VALUE, imax = 1, imin = 1;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] < 0){
+                int tmp = imax;
+                imax = imin;
+                imin = tmp;
+            }
+            imax = Math.max(imax*nums[i], nums[i]);
+            imin = Math.min(imin*nums[i], nums[i]);
+            
+            max = Math.max(max, imax);
+        }
+        return max;
+    }
+}*/
+        //Another dp approach similar to the one above
+        //by leetcode's compound
+        //Here we make use of an array maxherepre and minherepre which exemplifies the
+        //dp better than above (but the arrays are not necessary, as seen above)
+        //We need to calculate maximum and minimum for every position since a negative 
+        //number will make a high number (max) a small number (min), but with another
+        //negative could become the biggest number
+        //So for every position we calculate the biggest number (previous min and max) multiplied
+        //by the current number, and we get the max and min for every position, and then compare it
+        //to max so far 
+public int maxProduct(int[] A) {
+    if (A.length == 0) {
+        return 0;
+    }
+    
+    int maxherepre = A[0];
+    int minherepre = A[0];
+    int maxsofar = A[0];
+    int maxhere, minhere;
+    
+    for (int i = 1; i < A.length; i++) {
+        maxhere = Math.max(Math.max(maxherepre * A[i], minherepre * A[i]), A[i]);
+        minhere = Math.min(Math.min(maxherepre * A[i], minherepre * A[i]), A[i]);
+        maxsofar = Math.max(maxhere, maxsofar);
+        maxherepre = maxhere;
+        minherepre = minhere;
+    }
+    return maxsofar;
 }
