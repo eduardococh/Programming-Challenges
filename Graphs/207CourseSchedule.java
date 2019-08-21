ae2533fe2ee0013452e2c6b8e4bbe427c8db4dc0
Eduardo Contreras <eduardoverd_@hotmail.com>
2019-08-20 23:24:43 -0500

    leetcode.com/problems/course-schedule/

--- Graphs/207CourseSchedule.java
+++ Graphs/207CourseSchedule.java
@@ -1,3 +1,16 @@
+        //My solution
+        //It works and is not that complex
+        //The key to the code is the usage of bloquedCourses hashMap
+        //If a course has prerequisites it will appear, otherwise it will not appear
+        //So we now check through every course in bloquedCourses and start solving their
+        //Required courses until all courses are removed, meaning they can be done or until 
+        //we reach a cycle, detected by the traversedCourses list
+        //Bad runtime of 71ms better than only 11.99% O(N) where N is the number of prerequisites
+        //first process al N prerequisites and then in classSolver we eliminate requirements one by one
+        //so we visit them only once, if we visit one twice we break
+        //Bad memory of 53.1mb better than 27.69% O(N) where N is the number of prerequisites
+        //Since we store at most N prerequisites in the hashMap sets
+        //I could be wrong in these complexities ?¿
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         HashMap<Integer,HashSet<Integer>> bloquedCourses=new HashMap<>();
@@ -20,17 +33,19 @@ class Solution {
     }
     
     private boolean classSolver(int currentCourse,ArrayList<Integer> traversedCourses,HashMap<Integer,HashSet<Integer>> bloquedCourses,ArrayList<Integer> bloqCoursesList){
+        //System.out.println("WE enter forerr "+currentCourse);
         if(traversedCourses.contains(currentCourse)) return false;
+        traversedCourses.add(currentCourse);
         //System.out.println("WE enter for "+currentCourse);
         HashSet<Integer> prereq=bloquedCourses.get(currentCourse);
         //System.out.println("onbj "+prereq);
         for(Integer num:prereq){
             if(bloquedCourses.containsKey(num)){//If this num has a prerequisite
-                traversedCourses.add(num);
+                //System.out.println("testing "+num);
                 if(!classSolver(num,traversedCourses,bloquedCourses,bloqCoursesList)){
                     return false;
                 }
-                traversedCourses.remove(Integer.valueOf(num));
+                //traversedCourses.remove(Integer.valueOf(num));
             }//else no prerequisite for this num
         }
         bloquedCourses.remove(currentCourse);
