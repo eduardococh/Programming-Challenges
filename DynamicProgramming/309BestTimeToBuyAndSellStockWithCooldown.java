19eaa1f8fc7af0380435c36f980d8ecdcbb64854
Eduardo Contreras <eduardoverd_@hotmail.com>
2019-09-04 23:37:19 -0500

    leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/

--- DynamicProgramming/309BestTimeToBuyAndSellStockWithCooldown.java
+++ DynamicProgramming/309BestTimeToBuyAndSellStockWithCooldown.java
@@ -9,12 +9,38 @@ class Solution {
             this.status=status; 
         }
     }
-    
+     
     public int maxProfit(int[] prices) {
-        StockStatus dp[]=new StockStatus[prices.length+1];
-        dp[0]=new StockStatus(prices[0],true);
-        for(int i=0;i<prices;i++){
-            if(prices[i]<prices[i-1])
+        if(prices.length==0) return 0;
+        StockStatus dp[]=new StockStatus[prices.length];
+        dp[0]=new StockStatus(0,true);
+        int smaller=prices[0];
+        int smallerIndex=0;
+        for(int i=1;i<prices.length;i++){
+            if(prices[i]<prices[i-1]){
+                //we're smaller than previous there's no way to sell here
+                //we MUST buy now
+                //if(dp[i-1].status==false){
+                    dp[i]=new StockStatus(i-2>=0?dp[i-2].maxProfit:0,true);
+                    //System.out.println(i+" we put buy "+(i-2>=0?dp[i-2].maxProfit:0));
+                //}else{
+                    //dp[i]=new StockStatus(dp[i-1].maxProfit,true);
+                //}
+                smaller=prices[i];
+                smallerIndex=i;
+            }else{
+                //we're bigger than the previous, now calculate the profit
+                //from the smaller
+                dp[i]=new StockStatus(dp[smallerIndex].maxProfit+prices[i]-smaller,false);
+                //System.out.println(i+" we put sell "+(dp[smallerIndex].maxProfit+prices[i]-smaller));
+            }
         }
+        int maximum=0;
+        for(int i=0;i<prices.length;i++){
+            if(dp[i].maxProfit>maximum){
+                maximum=dp[i].maxProfit;
+            }
+        }
+        return maximum;
     }
 }
