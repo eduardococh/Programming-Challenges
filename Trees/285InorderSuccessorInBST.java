3fd71e268a348e39e7a29507321c71ec62d3c003
Eduardo Contreras <eduardoverd_@hotmail.com>
2019-09-10 23:47:28 -0500

    https://leetcode.com/problems/inorder-successor-in-bst/

--- Trees/285InorderSuccessorInBST.java
+++ Trees/285InorderSuccessorInBST.java
@@ -1,3 +1,12 @@
+        //My solution
+        //Bad runtime O(N) better than only 23%
+        //Bad memory
+        //Basically do an inorder traversal, when you find the node 
+        //go get the next node in the traversal
+        //It is a bad solution because we do not use the BST property of
+        //natural ordering, left are smaller right are bigger
+        //My code will go through nodes in a traversal, which is not needed
+        //as it can be seen in the second solution, which goes straight to the solution
 class Solution {
     
     class SearchStatus{
@@ -46,5 +55,71 @@ class Solution {
         if(right!=null && right.status==true) return new SearchStatus(true,null);
         
         return new SearchStatus(false,null);
+    }
+}
+
+        //Leetcode's solution, here we consider two scenarios
+        //one, p has right child, so the leftmost of this right child is our answer
+        //two, p has not right child, so we do an inorder traversal (like in my solution)
+        //and save the traversal in a stack, when 
+class Solution {
+  public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+    // the successor is somewhere lower in the right subtree
+    // successor: one step right and then left till you can
+    if (p.right != null) {
+      p = p.right;
+      while (p.left != null) p = p.left;
+      return p;
+    }
+
+    // the successor is somewhere upper in the tree
+    ArrayDeque<TreeNode> stack = new ArrayDeque<>();
+    int inorder = Integer.MIN_VALUE;
+
+    // inorder traversal : left -> node -> right
+    while (!stack.isEmpty() || root != null) {
+      // 1. go left till you can
+      while (root != null) {
+        stack.push(root);
+        root = root.left;
+      }
+
+      // 2. all logic around the node
+      root = stack.pop();
+      // if the previous node was equal to p
+      // then the current node is its successor
+      if (inorder == p.val) return root;
+      inorder = root.val;
+
+      // 3. go one step right
+      root = root.right;
+    }
+
+    // there is no successor
+    return null;
+  }
+}
+
+        //From leetcode's 0ms samples
+        //Super simple and elegant solution
+        //Amazing runtime 2ms O(N) better than 100%
+        //Go to node, if node is smaller or equal than p return the sucessor of right
+        //if node is bigger than p go to the left 
+        //
+class Solution {
+    
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        return successor(root, p);
+    }
+
+    public TreeNode successor(TreeNode root, TreeNode p) {
+        if (root == null) return null;
+
+        if (root.val <= p.val) {
+            return successor(root.right, p);
+         } else {
+            TreeNode left = successor(root.left, p);
+            return (left != null) ? left : root;
+          }
     }
 }
