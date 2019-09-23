dbdb46424a99ab4377aeb4f9d9df2aef6d7dee9b
Eduardo Contreras <eduardoverd_@hotmail.com>
2019-09-22 23:16:12 -0500

    leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/

--- DynamicProgramming/309BestTimeToBuyAndSellStockWithCooldown.java
+++ DynamicProgramming/309BestTimeToBuyAndSellStockWithCooldown.java
@@ -1,46 +1,27 @@
+        //Working solution from leetcode's 1ms sample
 class Solution {
-    
-    class StockStatus{
-        int maxProfit;
-        boolean status;//true-buy false-sell
-        
-        public StockStatus(int maxProfit,boolean status){
-            this.maxProfit=maxProfit;
-            this.status=status; 
-        }
-    }
-     
     public int maxProfit(int[] prices) {
-        if(prices.length==0) return 0;
-        StockStatus dp[]=new StockStatus[prices.length];
-        dp[0]=new StockStatus(0,true);
-        int smaller=prices[0];
-        int smallerIndex=0;
-        for(int i=1;i<prices.length;i++){
-            if(prices[i]<prices[i-1]){
-                //we're smaller than previous there's no way to sell here
-                //we MUST buy now
-                //if(dp[i-1].status==false){
-                    dp[i]=new StockStatus(i-2>=0?dp[i-2].maxProfit:0,true);
-                    //System.out.println(i+" we put buy "+(i-2>=0?dp[i-2].maxProfit:0));
-                //}else{
-                    //dp[i]=new StockStatus(dp[i-1].maxProfit,true);
-                //}
-                smaller=prices[i];
-                smallerIndex=i;
-            }else{
-                //we're bigger than the previous, now calculate the profit
-                //from the smaller
-                dp[i]=new StockStatus(dp[smallerIndex].maxProfit+prices[i]-smaller,false);
-                //System.out.println(i+" we put sell "+(dp[smallerIndex].maxProfit+prices[i]-smaller));
-            }
+        if(prices==null || prices.length == 0) return 0;
+        int n = prices.length;
+        int[] buy = new int[n+1];
+        int[] sell = new int[n+1];
+        buy[1] = -prices[0];
+        
+        /* day i 
+            buy 1. do nothing buy[i-1]
+                2. buy after sell, becuase of cooldown, sell[i-2]-prices[i] the current profix
+                max(1,2)
+            sell 1. do nothing sell[i-1]
+                 2. buy after sell, prices[i]+buy[i-1] since buy is negative
+                max(1,2)
+                 
+            buy[i] = max(sell[i-2]-price, buy[i-1])
+            sell[i] = max(buy[i-1]+price, sell[i-1])
+        */
+        for(int i =2;i<n+1;i++){
+            buy[i] = Math.max(sell[i-2]-prices[i-1], buy[i-1]);
+            sell[i] = Math.max(buy[i-1]+prices[i-1], sell[i-1]);
         }
-        int maximum=0;
-        for(int i=0;i<prices.length;i++){
-            if(dp[i].maxProfit>maximum){
-                maximum=dp[i].maxProfit;
-            }
-        }
-        return maximum;
+        return sell[n];
     }
 }
