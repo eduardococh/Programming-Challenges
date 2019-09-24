077e5f6aa5cacfec6e5fb000b62653948d910bab
Eduardo Contreras <eduardoverd_@hotmail.com>
2019-09-23 23:18:49 -0500

    leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/

--- DynamicProgramming/309BestTimeToBuyAndSellStockWithCooldown.java
+++ DynamicProgramming/309BestTimeToBuyAndSellStockWithCooldown.java
@@ -1,4 +1,24 @@
-        //Working solution from leetcode's 1ms sample
+        //Working solution from leetcode's 1'ms sample
+        //Using O(N) space
+        //We have two arrays, they work like this
+
+        /* For any day i
+            BUY 1. We do nothing, which means we grab buy in [i-1]
+                2. We buy after sell, because of cooldown, sell[i-2]-prices[i] the current profit
+                we save the max between the two options
+            SELL 1. Do nothing, we grab sell in [i-1]
+                 2. buy after sell, prices[i]+buy[i-1] since buy is negative
+                we save the max between the two options
+                 
+            buy[i] = max(sell[i-2]-price, buy[i-1])
+            sell[i] = max(buy[i-1]+price, sell[i-1])
+
+            EXAMPLE
+            [3, 5, 4, 8, 9, 5, 6, 1, 0, 2, 8]
+        BUY [0,-3,-3,-3,-3, 0, 0, 5, 6, 6, 6]
+        SELL[0, 0, 2, 2, 5, 6, 6, 6, 6, 8, 14]    
+        */
+
 class Solution {
     public int maxProfit(int[] prices) {
         if(prices==null || prices.length == 0) return 0;
@@ -6,18 +26,6 @@ class Solution {
         int[] buy = new int[n+1];
         int[] sell = new int[n+1];
         buy[1] = -prices[0];
-        
-        /* day i 
-            buy 1. do nothing buy[i-1]
-                2. buy after sell, becuase of cooldown, sell[i-2]-prices[i] the current profix
-                max(1,2)
-            sell 1. do nothing sell[i-1]
-                 2. buy after sell, prices[i]+buy[i-1] since buy is negative
-                max(1,2)
-                 
-            buy[i] = max(sell[i-2]-price, buy[i-1])
-            sell[i] = max(buy[i-1]+price, sell[i-1])
-        */
         for(int i =2;i<n+1;i++){
             buy[i] = Math.max(sell[i-2]-prices[i-1], buy[i-1]);
             sell[i] = Math.max(buy[i-1]+prices[i-1], sell[i-1]);
