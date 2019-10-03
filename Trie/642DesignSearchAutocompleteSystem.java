1ac6ae9a05744a199c622c61cb17db903e92e5e1
Eduardo Contreras <eduardoverd_@hotmail.com>
2019-10-02 19:58:29 -0500

    leetcode.com/problems/design-search-autocomplete-system/

--- Trie/642DesignSearchAutocompleteSystem.java
+++ Trie/642DesignSearchAutocompleteSystem.java
@@ -1,5 +1,23 @@
-
+        //Solution by leetcode
+        //The only solution I'm convering for now since it is a complex algorithm
+        //Bad runtime at 239ms better than only 28.68% 
+        //Autocomplete() O(K*L) L sentences of length K
+        //Input() O(P+Q+M*Log(M))
+        //Good memory better than 86.96% 
+        //My algorithm did the trie part great, the failure was to filter them to the final 3 results
+        //This is because I used a hashMap to store results, and it could have worked, but it
+        //added a lot of complexity, whereas using a custom object Node made the task easier here
+        //Here lookup method returns ALL matches, we save them to a list
+        //Then we sort that list using a 2 steps comparator, first we ask if A score is equal to B score
+        //we sort them using sentence.compareTo(sentence), 
+        //else we use b.times-a.times (this b-a order is important), if we reverse it then sorting is descending
+        //Once the list is ordered in score and ASCII order we 
+        //just loop for MIN(3 or list.size()) (3 or less iterations)
+        //and return that
 public class AutocompleteSystem {
+
+    Trie root;
+    String cur_sent = "";
 
     class Node {
         
@@ -16,19 +34,45 @@ public class AutocompleteSystem {
         int times;
         Trie[] branches = new Trie[27];
     }
+    
+    public AutocompleteSystem(String[] sentences, int[] times) {
+        root = new Trie();
+        for (int i = 0; i < sentences.length; i++) {
+            insert(root, sentences[i], times[i]);
+        }
+    }
+
+    public List<String> input(char c) {
+        List < String > res = new ArrayList < > ();
+        if (c == '#') { 
+            insert(root, cur_sent, 1);
+            cur_sent = "";
+        } else {
+            cur_sent += c;
+            List < Node > list = lookup(root, cur_sent);
+            //Custom comparator, if times are equal, solve by smaller ASCII code
+            Collections.sort(list, (a, b) -> a.times == b.times ? a.sentence.compareTo(b.sentence) : b.times - a.times);
+            for (int i = 0; i < Math.min(3, list.size()); i++)
+                res.add(list.get(i).sentence);
+        }
+        return res;
+    }
+
+    public void insert(Trie t, String s, int times) {
+        for (int i = 0; i < s.length(); i++) {
+            if (t.branches[int_(s.charAt(i))] == null){
+                t.branches[int_(s.charAt(i))] = new Trie();
+            }
+            t = t.branches[int_(s.charAt(i))];
+        }
+        t.times += times;
+    }
 
     public int int_(char c) {
         return c == ' ' ? 26 : c - 'a';
     }
 
-    public void insert(Trie t, String s, int times) {
-        for (int i = 0; i < s.length(); i++) {
-            if (t.branches[int_(s.charAt(i))] == null)
-                t.branches[int_(s.charAt(i))] = new Trie();
-            t = t.branches[int_(s.charAt(i))];
-        }
-        t.times += times;
-    }
+    //lookup get the trie to the base leaf of this sentence
     public List < Node > lookup(Trie t, String s) {
         List < Node > list = new ArrayList < > ();
         for (int i = 0; i < s.length(); i++) {
@@ -39,39 +83,24 @@ public class AutocompleteSystem {
         traverse(s, t, list);
         return list;
     }
+
+
+    //traverse get's all matches
     public void traverse(String s, Trie t, List < Node > list) {
+        //only if t is sentence end will it have times bigger than 0
         if (t.times > 0)
             list.add(new Node(s, t.times));
         for (char i = 'a'; i <= 'z'; i++) {
             if (t.branches[i - 'a'] != null)
                 traverse(s + i, t.branches[i - 'a'], list);
         }
+        //special case for space, a-z are treated in above loop 
         if (t.branches[26] != null)
             traverse(s + ' ', t.branches[26], list);
     }
-    Trie root;
-    public AutocompleteSystem(String[] sentences, int[] times) {
-        root = new Trie();
-        for (int i = 0; i < sentences.length; i++) {
-            insert(root, sentences[i], times[i]);
-        }
-    }
-    String cur_sent = "";
-    public List < String > input(char c) {
-        List < String > res = new ArrayList < > ();
-        if (c == '#') { 
-            insert(root, cur_sent, 1);
-            cur_sent = "";
-        } else {
-            cur_sent += c;
-            List < Node > list = lookup(root, cur_sent);
-            Collections.sort(list, (a, b) -> a.times == b.times ? a.sentence.compareTo(b.sentence) : b.times - a.times);
-            for (int i = 0; i < Math.min(3, list.size()); i++)
-                res.add(list.get(i).sentence);
-        }
-        return res;
-    }
-}       
+}
+
+
 
 
         //My attempt at an approach DOES NOT WORK
