c0458bd63be6d8ac137eac1ec8153777f4f83208
Eduardo Contreras <eduardoverd_@hotmail.com>
2019-10-08 23:30:42 -0500

    leetcode.com/problems/wildcard-matching/

--- DynamicProgramming/44WildcardMatching.java
+++ DynamicProgramming/44WildcardMatching.java
@@ -1,5 +1,102 @@
+        //
+class Solution {
+    public boolean isMatch(String s, String p) {
+    int sLen = s.length(), pLen = p.length();
+    int sIdx = 0, pIdx = 0;
+    int starIdx = -1, sTmpIdx = -1;
+
+    while (sIdx < sLen) {
+      // If the pattern caracter = string character
+      // or pattern character = '?'
+      if (pIdx < pLen && (p.charAt(pIdx) == '?' || p.charAt(pIdx) == s.charAt(sIdx))){
+        ++sIdx;
+        ++pIdx;
+      }
+      // If pattern character = '*'
+      else if (pIdx < pLen && p.charAt(pIdx) == '*') {
+        // Check the situation
+        // when '*' matches no characters
+        starIdx = pIdx;
+        sTmpIdx = sIdx;
+        ++pIdx;
+      }
+      // If pattern character != string character
+      // or pattern is used up
+      // and there was no '*' character in pattern 
+      else if (starIdx == -1) {
+        return false;
+      }
+      // If pattern character != string character
+      // or pattern is used up
+      // and there was '*' character in pattern before
+      else {
+        // Backtrack: check the situation
+        // when '*' matches one more character
+        pIdx = starIdx + 1;
+        sIdx = sTmpIdx + 1;
+        sTmpIdx = sIdx;
+      }
+    }
+
+    // The remaining characters in the pattern should all be '*' characters
+    for(int i = pIdx; i < pLen; i++)
+      if (p.charAt(i) != '*') return false;
+    return true;
+    }
+}       
+        //Leetcode's solution
+        //Good runtime at 6ms better than 76.67%
+        //Amazing memory better than 100% 
+class Solution {
+  public boolean isMatch(String s, String p) {
+    int sLen = s.length(), pLen = p.length();
+
+    // base cases
+    if (p.equals(s) || p.equals("*")) return true;
+    if (p.isEmpty() || s.isEmpty()) return false;
+
+    // init all matrix except [0][0] element as False
+    boolean[][] d = new boolean[pLen + 1][sLen + 1];
+    d[0][0] = true;
+
+    // DP compute
+    for(int pIdx = 1; pIdx < pLen + 1; pIdx++) {
+      // the current character in the pattern is '*'
+      if (p.charAt(pIdx - 1) == '*') {
+        int sIdx = 1;
+        // d[p_idx - 1][s_idx - 1] is a string-pattern match
+        // on the previous step, i.e. one character before.
+        // Find the first idx in string with the previous math.
+        while ((!d[pIdx - 1][sIdx - 1]) && (sIdx < sLen + 1)) sIdx++;
+        // If (string) matches (pattern),
+        // when (string) matches (pattern)* as well
+        d[pIdx][sIdx - 1] = d[pIdx - 1][sIdx - 1];
+        // If (string) matches (pattern),
+        // when (string)(whatever_characters) matches (pattern)* as well
+        while (sIdx < sLen + 1) d[pIdx][sIdx++] = true;
+      }
+      // the current character in the pattern is '?'
+      else if (p.charAt(pIdx - 1) == '?') {
+        for(int sIdx = 1; sIdx < sLen + 1; sIdx++)
+          d[pIdx][sIdx] = d[pIdx - 1][sIdx - 1];
+      }
+      // the current character in the pattern is not '*' or '?'
+      else {
+        for(int sIdx = 1; sIdx < sLen + 1; sIdx++) {
+          // Match is possible if there is a previous match
+          // and current characters are the same
+          d[pIdx][sIdx] = d[pIdx - 1][sIdx - 1] &&
+                  (p.charAt(pIdx - 1) == s.charAt(sIdx - 1));
+        }
+      }
+    }
+    return d[pLen][sLen];
+  }
+}
+
         //My Solution
         //Works but time limit exceeded
+        //A way to memoitize or add dp into it should work
 class Solution {
     public boolean isMatch(String s, String p) {
         if(p.length()==1 && p.charAt(0)=='*') return true;
