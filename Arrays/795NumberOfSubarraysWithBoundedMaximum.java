a4437ad45491a748c8403bf45da438e5cf52ad9c
Eduardo Contreras <eduardoverd_@hotmail.com>
2019-10-17 23:38:40 -0500

    leetcode.com/problems/number-of-subarrays-with-bounded-maximum/

--- Arrays/795NumberOfSubarraysWithBoundedMaximum.java
+++ Arrays/795NumberOfSubarraysWithBoundedMaximum.java
@@ -4,9 +4,10 @@ class Solution {
         int result=0;
         for(int i=0;i<A.length;i++){
             if(A[i]>R){
-                System.out.println("we enter "+startIndex+" "+i);
-                result+=resolveSubarray(A,startIndex,i,L);
+                result+=resolveSubarray(A,startIndex,i-1,L);
                 startIndex=i+1;
+            }else if(i==A.length-1){
+                result+=resolveSubarray(A,startIndex,i,L);
             }
         }
         
@@ -15,26 +16,47 @@ class Solution {
 
     public int resolveSubarray(int[] array,int startIndex,int endIndex, int L){
         double result=0;
-        int N=(endIndex-startIndex);
+        int N=(endIndex-startIndex)+1;
         result+=(N*(N+1))/2;
         boolean smallerNum=false;
         int numberOfSmaller=0;
-        //System.out.println("our num is "+result);
-        for(int i=startIndex;i<endIndex;i++){
+        for(int i=startIndex;i<=endIndex;i++){
             if(array[i]<L){
                 numberOfSmaller++;
+                smallerNum=true;
             }else{
                 if(smallerNum){
                     N=numberOfSmaller;
                     result-=(N*(N+1))/2;
-                    System.out.println("minus "+(N*(N+1))/2);
                     numberOfSmaller=0;
                     smallerNum=false;
                 }
             }
         }
+        if(smallerNum){
+            N=numberOfSmaller;
+            result-=(N*(N+1))/2;
+            numberOfSmaller=0;
+            smallerNum=false;
+        }
         return (int)result;
     }
 
 
+}
+
+
+class Solution {
+    public int numSubarrayBoundedMax(int[] A, int L, int R) {
+        return count(A, R) - count(A, L - 1);
+    }
+    
+    public int count(int[] A, int bound) {
+        int cur = 0, res = 0;
+        for (int num : A) {
+            cur = num <= bound ? cur + 1 : 0;
+            res += cur;
+        }
+        return res;
+    }
 }
