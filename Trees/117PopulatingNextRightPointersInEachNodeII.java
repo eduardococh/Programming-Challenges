4ea6f03eff4bfbd72bd74507467b2a0f8ef15d22
Eduardo Contreras <eduardoverd_@hotmail.com>
2019-10-22 21:10:50 -0500

    leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/

--- Trees/117PopulatingNextRightPointersInEachNodeII.java
+++ Trees/117PopulatingNextRightPointersInEachNodeII.java
@@ -1,54 +1,37 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public Node left;
-    public Node right;
-    public Node next;
-
-    public Node() {}
-
-    public Node(int _val,Node _left,Node _right,Node _next) {
-        val = _val;
-        left = _left;
-        right = _right;
-        next = _next;
-    }
-};
-*/
+        //From leetcode's 0ms solution
+        //Amazing runtime 0ms better than 100% O(N)
+        //Amazing memory better than 92.86% O(1) (problem specifies stack does not count)
+        //the trick is using temp as the start of every level and curr and root as iterators
 class Solution {
     public Node connect(Node root) {
-        if(root==null) return null;
-        if(root.left!=null && root.right!=null){//none is null
-            System.out.println("node "+root.left.val+" has next "+root.right.val);
-            root.left.next=root.right;
-        }
-        //i have next node
-        if(root.next!=null){
-            if(root.right!=null){
-                assignNext(root,true);
-            }else if(root.left!=null){
-                assignNext(root,false);
-            }
-        }
-        connect(root.left);
-        connect(root.right);
-        return root;
-    }
     
-    public void assignNext(Node root,boolean next){
-        if(next){
-            if(root.next.left!=null){
-                root.right.next=root.next.left;
-            }else if(root.next.right!=null){
-                root.right.next=root.next.right;
-            }//else 
-        }else{
-            if(root.next.left!=null){
-                root.left.next=root.next.left;
-            }else if(root.next.right!=null){
-                root.left.next=root.next.right;
-            }//else 
+        if(root==null )
+            return root;
+        
+        Node ans =root;
+            
+        Node temp = new Node(0);
+        
+        Node cur =temp;
+        while(root!=null){
+
+            while(root!=null){
+
+                if(root.left!=null){
+                    cur.next = root.left;
+                    cur=cur.next;
+                }
+                if(root.right!=null){
+                    cur.next = root.right;
+                    cur = cur.next;
+                }
+                root=root.next;
+
+            }
+            root=temp.next;
+            cur=temp;
+            cur.next=null;
         }
+        return ans;
     }
 }
