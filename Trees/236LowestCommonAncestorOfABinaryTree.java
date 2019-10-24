692ca89b029a4e2dfaffc3bfecc1aea660ca866b
Eduardo Contreras <eduardoverd_@hotmail.com>
2019-10-23 22:22:41 -0500

    class Solution {

--- Trees/236LowestCommonAncestorOfABinaryTree.java
+++ Trees/236LowestCommonAncestorOfABinaryTree.java
@@ -62,6 +62,27 @@ class Solution {
         return left == null ? right : left;
     }
 }
+        //Same approach as above but with my own logic
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null) return null;
+        
+        if(root.val==p.val || root.val==q.val){
+            return root;
+        }
+        
+        TreeNode left=lowestCommonAncestor(root.left,p,q);
+        TreeNode right=lowestCommonAncestor(root.right,p,q);
+        
+        if(left!=null && right!=null) return root;
+        
+        if(left!=null) return left;
+        if(right!=null) return right;
+        
+        
+        return null;
+    }
+}
 
 
         //Iterative solution
