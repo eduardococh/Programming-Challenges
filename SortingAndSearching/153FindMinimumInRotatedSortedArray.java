e5f4e9933e28962cdceafe371a77b151558e0cde
Eduardo Contreras <eduardoverd_@hotmail.com>
2019-10-28 19:56:48 -0600

    https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/

--- SortingAndSearching/153FindMinimumInRotatedSortedArray.java
+++ SortingAndSearching/153FindMinimumInRotatedSortedArray.java
@@ -6,7 +6,8 @@
         //Here we compare nums[middle] to nums[right], if we are bigger than means that the smaller
         //is to our right, if we are smaller than nums[right] that means the smaller number could
         //the to our left or could be middle 
-        //
+        //It could be done the other way (comparing with element at 0 as leetcode
+        //solution works, but I think is a complex approach, not worth viewing)
 class Solution {
     public int findMin(int[] nums) {
         if(nums.length==0) return 0;
@@ -22,34 +23,4 @@ class Solution {
         }
         return nums[low];
     }
-}
-
-
-class Solution {
-  public int findMin(int[] nums) { 
-    if (nums.length == 1) {
-      return nums[0];
-    }
-    int left = 0, right = nums.length - 1;
-    if (nums[right] > nums[0]) {
-      return nums[0];
-    }
-    
-    // Binary search way
-    while (right >= left) {
-      int mid = left + (right - left) / 2;
-      if (nums[mid] > nums[mid + 1]) {
-        return nums[mid + 1];
-      }
-      if (nums[mid - 1] > nums[mid]) {
-        return nums[mid];
-      }
-      if (nums[mid] > nums[0]) {
-        left = mid + 1;
-      } else {
-        right = mid - 1;
-      }
-    }
-    return -1;
-  }
 }
