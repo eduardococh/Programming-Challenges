858d4d5d732e25f56c31f602f16e5d11f2e58c7b
Eduardo Contreras <eduardoverd_@hotmail.com>
2019-10-31 23:30:38 -0600

    leetcode.com/problems/implement-trie-prefix-tree/

--- Trie/208ImplementTrie.java
+++ Trie/208ImplementTrie.java
@@ -1,3 +1,7 @@
+        //My solution
+        //Good runtime 75ms better than 81.31% 
+        //Runtimes vary
+        //
 class Trie {
     
     Trie next[];
@@ -65,10 +69,44 @@ class Trie {
     }
 }
 
-/**
- * Your Trie object will be instantiated and called as such:
- * Trie obj = new Trie();
- * obj.insert(word);
- * boolean param_2 = obj.search(word);
- * boolean param_3 = obj.startsWith(prefix);
- */
+ class Trie {
+    Trie[] next = new Trie[26];
+    String word;
+    /** Initialize your data structure here. */
+    public Trie() {
+        
+    }
+    
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        if(word==null) return;
+        Trie curr = this;
+        for(char c:word.toCharArray()){
+            if(curr.next[c-'a']==null){
+                curr.next[c-'a'] = new Trie();
+            }
+            curr = curr.next[c-'a'];
+        }
+        curr.word = word;
+    }
+    
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        Trie curr = this;
+        for(char c: word.toCharArray()){
+            if(curr.next[c-'a']==null) return false;
+            curr= curr.next[c-'a'];
+        }
+        return curr.word!=null;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        Trie curr = this;
+        for(char c: prefix.toCharArray()){
+            if(curr.next[c-'a']==null) return false;
+            curr = curr.next[c-'a'];
+        }
+        return true;
+    }
+}
