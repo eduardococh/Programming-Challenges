a7bd3d92a2e9a28621461d72802b4f0e57174590
Eduardo Contreras <eduardoverd_@hotmail.com>
2019-11-03 12:50:03 -0600

    leetcode.com/problems/clone-graph/

---
+++ Graphs/133CloneGraph.java
@@ -0,0 +1,54 @@
+        //My solution using recursion and a map
+        //Amazing runtime 1ms better than 100% O(N)
+        //Good memory better than 80% O(N)
+        //Simple solution a lot like cloning a tree
+class Solution {
+    
+    HashMap<Node,Node> mapping=new HashMap<Node,Node>();
+    
+    public Node cloneGraph(Node node) {
+        if(node==null) return null;
+        
+        if(mapping.containsKey(node)) return mapping.get(node);
+        
+        Node newNode=new Node(node.val);
+        newNode.neighbors=new ArrayList<Node>();
+        mapping.put(node,newNode);
+        
+        int len=node.neighbors.size();
+        for(int i=0;i<len;i++){
+            newNode.neighbors.add(cloneGraph(node.neighbors.get(i)));
+        }
+        
+        return newNode;
+    }
+}
+
+        //Iterative solution from leetcode's 2ms samples
+        //Runtime a little worse than with recursion
+class Solution {
+    public Node cloneGraph(Node node) {
+        if (node == null) {
+            return null;
+        }
+        Queue<Node> queue = new LinkedList<Node>();
+        HashMap<Node, Node> map = new HashMap<>();
+        queue.offer(node);
+        map.put(node, createNewNode(node));
+        while(!queue.isEmpty()) {
+            Node currentNode = queue.poll();
+            for (Node neighbor: currentNode.neighbors) {
+                if (!map.containsKey(neighbor)) {
+                    map.put(neighbor, createNewNode(neighbor));
+                    queue.offer(neighbor);
+                } 
+                map.get(currentNode).neighbors.add(map.get(neighbor));
+            }
+        }
+        return map.get(node);
+    }
+    
+    private Node createNewNode(Node node) {
+        return new Node(node.val, new ArrayList<Node>());
+    }
+}
