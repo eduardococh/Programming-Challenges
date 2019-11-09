86fa511eb3d0a8a02d154ab44d61e3f5cce93a31
Eduardo Contreras <eduardoverd_@hotmail.com>
2019-11-09 10:14:29 -0600

    leetcode.com/problems/gas-station/

--- Arrays/134GasStation.java
+++ Arrays/134GasStation.java
@@ -1,57 +1,56 @@
+        //My solution
+        //Amazing runtime 0ms better than 100% O(N)
+        //Good runtime better than 74.51% O(1)
+        //It does one loop searching for a viable path
+        //if it finds a canditate, it goes to the second loop and confirms
+        //that it can be done, otherwise return 1
+        //Since the program guarantees that there's only one solution this 
+        //simple approach works (unlike finding the maximum for example)
 class Solution {
     public int canCompleteCircuit(int[] gas, int[] cost) {
         int len=gas.length;
-        int diff[]=new int[len];
-        int tgas=0,tcost=0;
-        for(int i=0;i<len;i++){
-            diff[i]=gas[i]-cost[i];
-            tgas+=gas[i];
-            tcost+=cost[i];
-        }
-        if(tcost>tgas) return -1;
-        int longestStreak=0,startingIL=0;
-        int currentStreak=0,startingIC=0;
-        for(int i=0;i<len;i++){
-            if(diff[i]>=0){
-                if(currentStreak==0) startingIC=i;
-                currentStreak+=diff[i];
-            }else{
-                if(currentStreak>0){
-                    if(currentStreak>longestStreak){
-                        longestStreak=currentStreak;
-                        startingIL=startingIC;
-                    }
-                    currentStreak=0;
-                    startingIC=0;
-                }
-            }
-        }
-        if(currentStreak>0){
-            if(currentStreak>longestStreak){
-                longestStreak=currentStreak;
-                startingIL=startingIC;
-            }
-        }
-        return startingIL;
-    }
-}
-
-class Solution {
-    public int canCompleteCircuit(int[] gas, int[] cost) {
-        int len=gas.length;
-        int diff[]=new int[len];
-        int tgas=0,tcost=0;
         int currentStreak=0;
         int startIndex=0;
         for(int i=0;i<len;i++){
             currentStreak=(currentStreak+gas[i]-cost[i]);
-            //System.out.println(currentStreak);
             if(currentStreak<0){
                 currentStreak=0;
                 startIndex=i+1;
             } 
         }
         if(startIndex==len+1) return -1;
+        for(int i=0;i<startIndex;i++){
+            currentStreak=(currentStreak+gas[i]-cost[i]);
+            if(currentStreak<0){
+                return -1;
+            } 
+        }
         return startIndex;
     }
+}
+
+        //Leetcode's solution
+        //Same runtime as my solution
+        //Does not need to do second loop, only get total gas amount and if you find a possible 
+        //station and total gas is positive return it
+class Solution {
+  public int canCompleteCircuit(int[] gas, int[] cost) {
+    int n = gas.length;
+
+    int total_tank = 0;
+    int curr_tank = 0;
+    int starting_station = 0;
+    for (int i = 0; i < n; ++i) {
+      total_tank += gas[i] - cost[i];
+      curr_tank += gas[i] - cost[i];
+      // If one couldn't get here,
+      if (curr_tank < 0) {
+        // Pick up the next station as the starting one.
+        starting_station = i + 1;
+        // Start with an empty tank.
+        curr_tank = 0;
+      }
+    }
+    return total_tank >= 0 ? starting_station : -1;
+  }
 }
