8e20649e85db4350650efbb213971a3fadbefa07
Eduardo Contreras <eduardoverd_@hotmail.com>
2019-11-12 23:49:51 -0600

    leetcode.com/problems/rectangle-area/

--- Math/223RectangleArea.java
+++ Math/223RectangleArea.java
@@ -1,8 +1,10 @@
+        //Pretty genius solution
+        //Get overlappings of squares
 class Solution {
     public int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
         
         int areaOfSqrA = (C-A) * (D-B);
-         int areaOfSqrB = (G-E) * (H-F);
+        int areaOfSqrB = (G-E) * (H-F);
         
         int left = Math.max(A, E);
         int right = Math.min(G, C);
