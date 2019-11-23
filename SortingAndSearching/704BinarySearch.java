d667404b0a91a8ec4f4f1033e2264a1dfc025c31
Eduardo Contreras <eduardoverd_@hotmail.com>
2019-11-22 21:26:52 -0600

    https://leetcode.com/problems/binary-search/

--- SortingAndSearching/704BinarySearch.java
+++ SortingAndSearching/704BinarySearch.java
@@ -1,10 +1,12 @@
         //My binary search approach
-        //Maybe a typical textbook implementation
+        //It is the same implementation as leetcode
         //Not a lot to comment
         //PERSONAL ERRORS
         //LOW<=HIGH since mid could fall in the middle
         //HIGH = LENGTH-1 avoid overflow
         //If it is not found in the loop return -1
+        //Runtime of 0ms better than 100% O(Log(N))
+        //Average memory better than 60%
 class Solution {
     public int search(int[] nums, int target) {
         int low=0,high=nums.length-1;
