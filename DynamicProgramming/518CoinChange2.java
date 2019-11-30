68cf8a33642825c4a9983842648cf7da6109e515
Eduardo Contreras <eduardoverd_@hotmail.com>
2019-11-29 20:44:42 -0600

    leetcode.com/problems/coin-change-2/

--- DynamicProgramming/518CoinChange2.java
+++ DynamicProgramming/518CoinChange2.java
@@ -1,24 +1,28 @@
-class Solution {
-    
-    HashSet<ArrayList<Integer>> res;
-    int nCoins[];
-    
-    public int change(int amount, int[] coins) {
-        res=new HashSet<ArrayList<Integer>>();
-        nCoins=new int[coins.length];
-        changeHel(amount,coins);
-        return res.length();
-    }
-    
-    public void changeHel(int amount,int[] coins){
-        if(amount==0){
-            ArrayList<Integer> tuple=new ArrayList<Integer>();
-            for(int coin:nCoins){
-                tuple.add(coin);
-            }
-            if(!res.contains(tuple)) res.add(tuple);
+    class Solution {
+
+        HashSet<ArrayList<Integer>> res;
+        int nCoins[];
+
+        public int change(int amount, int[] coins) {
+            res=new HashSet<ArrayList<Integer>>();
+            nCoins=new int[coins.length];
+            changeHel(amount,coins);
+            return res.size();
         }
-        if(amount<0) return;
-        for(int i=0
+
+        public void changeHel(int amount,int[] coins){
+            if(amount==0){
+                ArrayList<Integer> tuple=new ArrayList<Integer>();
+                for(int coin:nCoins){
+                    tuple.add(coin);
+                }
+                if(!res.contains(tuple)) res.add(tuple);
+            }
+            if(amount<0) return;
+            for(int i=0;i<coins.length;i++){
+                nCoins[i]++;
+                changeHel(amount-coins[i],coins);
+                nCoins[i]--;
+            }
+        }
     }
-}
