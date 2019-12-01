53af92b0e5ed5c7f11825ed67fbc10eaee3a9bb2
Eduardo Contreras <eduardoverd_@hotmail.com>
2019-11-30 19:59:51 -0600

    leetcode.com/problems/coin-change-2/

--- DynamicProgramming/518CoinChange2.java
+++ DynamicProgramming/518CoinChange2.java
@@ -1,28 +1,41 @@
-    class Solution {
+class Solution {
+    HashSet<ArrayList<Integer>> res;
+    int nCoins[];
 
-        HashSet<ArrayList<Integer>> res;
-        int nCoins[];
+    public int change(int amount, int[] coins) {
+        res=new HashSet<ArrayList<Integer>>();
+        nCoins=new int[coins.length];
+        changeHel(amount,coins);
+        return res.size();
+    }
 
-        public int change(int amount, int[] coins) {
-            res=new HashSet<ArrayList<Integer>>();
-            nCoins=new int[coins.length];
-            changeHel(amount,coins);
-            return res.size();
+    public void changeHel(int amount,int[] coins){
+        if(amount==0){
+            ArrayList<Integer> tuple=new ArrayList<Integer>();
+            for(int coin:nCoins){
+                tuple.add(coin);
+            }
+            if(!res.contains(tuple)) res.add(tuple);
         }
-
-        public void changeHel(int amount,int[] coins){
-            if(amount==0){
-                ArrayList<Integer> tuple=new ArrayList<Integer>();
-                for(int coin:nCoins){
-                    tuple.add(coin);
-                }
-                if(!res.contains(tuple)) res.add(tuple);
-            }
-            if(amount<0) return;
-            for(int i=0;i<coins.length;i++){
-                nCoins[i]++;
-                changeHel(amount-coins[i],coins);
-                nCoins[i]--;
-            }
+        if(amount<0) return;
+        for(int i=0;i<coins.length;i++){
+            nCoins[i]++;
+            changeHel(amount-coins[i],coins);
+            nCoins[i]--;
         }
     }
+}
+
+class Solution {
+  public int change(int amount, int[] coins) {
+    int[] dp = new int[amount + 1];
+    dp[0] = 1;
+
+    for (int coin : coins) {
+      for (int x = coin; x < amount + 1; ++x) {
+        dp[x] += dp[x - coin];
+      }
+    }
+    return dp[amount];
+  }
+}
