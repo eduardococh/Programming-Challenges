ceb7de1ab9cc236d8ec308f98391f3444ec61a26
Eduardo Contreras <eduardoverd_@hotmail.com>
2019-12-01 18:35:38 -0600

    leetcode.com/problems/coin-change-2/

--- DynamicProgramming/518CoinChange2.java
+++ DynamicProgramming/518CoinChange2.java
@@ -26,6 +26,11 @@ class Solution {
     }
 }
 
+
+        //Solution by leetcode's
+        //Runtime of 1ms better than 100%
+        //Memory of 34.4 mb better than 100%
+        //This solution is so good because it only uses a linear amount sized array
 class Solution {
   public int change(int amount, int[] coins) {
     int[] dp = new int[amount + 1];
@@ -38,4 +43,30 @@ class Solution {
     }
     return dp[amount];
   }
+}
+
+        //My solution based on b2b swe video
+        //Bad runtime of 8ms faster than only 27.64% O(M*N) M is amount and N num of coins
+        //Bad memory of 36.6mb less than 30.23% O(M*N)
+class Solution {
+    public int change(int amount, int[] coins) {
+        int[][] dp=new int[coins.length+1][amount+1];
+        //filling the cero amount column with one's (for the subproblem of cero dollars
+        //there's always one way of making it)
+        for(int i=0;i<=coins.length;i++){
+            dp[i][0]=1;
+        }
+        //row 0 will be full of 0's exept in the first position
+        for(int i=1;i<=coins.length;i++){
+            for(int j=0;j<=amount;j++){
+                //System.out.println("we in "+i+" "+j);
+                if(j>=coins[i-1]){
+                    dp[i][j]=dp[i-1][j]+dp[i][j-coins[i-1]];
+                }else{
+                    dp[i][j]=dp[i-1][j];
+                }
+            }
+        }
+        return dp[coins.length][amount];
+    }
 }
