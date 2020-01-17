99a6af36ae3b0a24ff3ab5c0a321174d6e2d8e0f
Eduardo Contreras <eduardoverd_@hotmail.com>
2020-01-16 20:40:39 -0600

    leetcode.com/problems/read-n-characters-given-read4/

--- Design/157ReadNCharactersGivenRead4.java
+++ Design/157ReadNCharactersGivenRead4.java
@@ -54,3 +54,53 @@ public class Solution extends Reader4 {
         return readBytes;
     }
 }
+
+
+public Class readN{
+	
+	int genIndex=0;
+	char r4[];
+	char remaining[];
+           int cC = 0;
+	readN(){
+		r4 = new char[];
+}
+
+int readN(char buf [ ], int n){
+
+	int thisRun=0;
+                                                               
+	while(c>0){
+		int index4=0;
+		if(cC == 0)
+			read4(r4);
+
+		while(cC > 0 && thisRun<n){
+buf[genIndex++]=r4[4-Cc];
+thisRun++;
+cC--;
+}
+
+
+if(thisRun>=n) {
+	break;
+}
+if(readen!=4){ 
+saveRemaining(index4,r4);
+	break;
+}
+readen=read4(r4);
+}
+return thisRun;
+}
+
+}
+
+saveRemaining(int index4,char[] r4){
+	remaining=new char[3];
+	int remainIndex=0;
+	while(index4<4){
+		remaining[remainIndex++]=r4[index4++];
+}
+return;
+}
