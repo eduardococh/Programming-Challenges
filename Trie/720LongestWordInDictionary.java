d32f423da22219979fd7ef598f6cbd18e9e562e9
Eduardo Contreras <eduardoverd_@hotmail.com>
2020-01-21 21:26:00 -0600

    leetcode.com/problems/longest-word-in-dictionary/

--- Trie/720LongestWordInDictionary.java
+++ Trie/720LongestWordInDictionary.java
@@ -60,3 +60,56 @@ class Solution {
         return result;
     }
 }
+
+
+        //My solution using Trie
+        //We're close to mastering trie, but still doing mistakes
+        //Big mistake today was in the creation of the trie
+        //When you create a new trie you DIDN'T GO NEXT, big mistake
+        //Bad solution runtime of 18ms better than only 30.95%
+        //STILL much better than leetcode's trie implementation (81ms better than 5.08%)
+        //Bad memory better than only 6.25%
+class Solution {
+    class Trie{
+        String eow;
+        Trie next[];
+        
+        Trie(){
+            eow="";
+            next=new Trie[26];
+        }
+    }
+    
+    public String longestWord(String[] words) {
+        Trie myTrie=new Trie();
+        for(String w:words){
+            addToTrie(w,myTrie);
+        }
+        return getLongest(myTrie);
+    }
+    
+    private void addToTrie(String w,Trie myTrie){
+        Trie mover=myTrie;
+        for(char c:w.toCharArray()){
+            if(mover.next[c-'a']==null){
+                mover.next[c-'a']=new Trie();
+                mover=mover.next[c-'a'];////BIGGGGGGGGGG ERRORRRRRR GOOOODDDDDDDDD
+            }else{
+                mover=mover.next[c-'a'];
+            }
+        }
+        mover.eow=w;
+    }
+    
+    private String getLongest(Trie myTrie){
+        String longest="";
+        for(int i=0;i<26;i++){
+            if(myTrie.next[i]!=null && myTrie.next[i].eow.length()>0){
+                if(myTrie.next[i].eow.length()>longest.length()) longest=myTrie.next[i].eow;
+                String curr=getLongest(myTrie.next[i]);
+                if(curr.length()>longest.length()) longest=curr;
+            }
+        }
+        return longest;
+    }
+}
