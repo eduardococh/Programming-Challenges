e90f3de13886708024d578502eda43cbe138e068
Eduardo Contreras <eduardoverd_@hotmail.com>
2020-01-25 15:37:43 -0600

    leetcode.com/problems/number-complement/

--- Math/476NumberComplement.java
+++ Math/476NumberComplement.java
@@ -1,13 +1,18 @@
+        //Genius bit operation by leetcode's hilloni
+        //Use an XOR with 1, any number XOR 1 gives the opposite number
+        //Any number XOR 0 gives the same number, so here we do an XOR
+        //bit by bit, we use x as our mask and shift to the right 1 by 1
+        //until we do this 32 times
+        //Runtime of 1ms better than 34%? O(N)
+        //Memory better than 100% O(1)
 class Solution {
     public int findComplement(int num) {
-   int x=1,i=1;
-    while(x<=num && i<32)
-    {
-        num=num^x;
-        x=x<<1;
-        i++;
+        int x=1,i=1;
+        while(x<=num && i<32){
+            num=num^x;
+            x=x<<1;
+            i++;
+        }
+        return num;
     }
-
-    return num;
-}
 }
