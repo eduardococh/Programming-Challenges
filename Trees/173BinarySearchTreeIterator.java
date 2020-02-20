b5262b226e11f7176c5d599ee1f614f97944cbe6
Eduardo Contreras <eduardoverd_@hotmail.com>
2020-02-19 23:12:40 -0600

    leetcode.com/problems/binary-search-tree-iterator/

--- Trees/173BinarySearchTreeIterator.java
+++ Trees/173BinarySearchTreeIterator.java
@@ -1,11 +1,8 @@
 /**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
+ *  My solution using mobile pointer, smallest/biggest variable and a stack of parents 
+ *  Good runtime 16ms better than 84.73% O(1) for hasNext() and O(N) amortized to O(1)
+ *  Amazing memory better than 100% O(h)
+ *  A little too complex compared with other solutions but satisfied with it
  */
 class BSTIterator {
     
@@ -39,28 +36,83 @@ class BSTIterator {
             smallest=exploring.val;
             return exploring.val;
         }
-        if(exploring.right==null){
-            return exploreTree(parents.pop());
-        }else{
+        if(exploring.right!=null && exploring.right.val>smallest){
             parents.push(exploring);
             return exploreTree(exploring.right);
+        }else{
+            return exploreTree(parents.pop());
         }
     }
     
     /** @return whether we have a next smallest number */
     public boolean hasNext() {
+        //if(current==null) return false;
         return current.val==biggest?false:true;
     }
     
     private int getBiggest(TreeNode root){
+        if(root==null) return -1;
         if(root.right==null) return root.val;
         return getBiggest(root.right);
     }
 }
 
+
+//Leetcode mentions the flatten tree approach, very simple but out of memory bounds
+//Submitted it and it actually has same runtime and memory as my approach
+
+
+
 /**
- * Your BSTIterator object will be instantiated and called as such:
- * BSTIterator obj = new BSTIterator(root);
- * int param_1 = obj.next();
- * boolean param_2 = obj.hasNext();
+ * Stack approach, just brilliant and elegant
+ * Interesting that it has bad runtime of 17ms, better than only 48.11% O(1) for has next
+ * O(N) for worst scenario of next(), but amortized to O(1)
+ * Amazing memory better than 100% O(h)
  */
+class BSTIterator {
+
+    Stack<TreeNode> stack;
+
+    public BSTIterator(TreeNode root) {
+        
+        // Stack for the recursion simulation
+        this.stack = new Stack<TreeNode>();
+        
+        // Remember that the algorithm starts with a call to the helper function
+        // with the root node as the input
+        this._leftmostInorder(root);
+    }
+
+    private void _leftmostInorder(TreeNode root) {
+      
+        // For a given node, add all the elements in the leftmost branch of the tree
+        // under it to the stack.
+        while (root != null) {
+            this.stack.push(root);
+            root = root.left;
+        }
+    }
+
+    /**
+     * @return the next smallest number
+     */
+    public int next() {
+        // Node at the top of the stack is the next smallest element
+        TreeNode topmostNode = this.stack.pop();
+
+        // Need to maintain the invariant. If the node has a right child, call the 
+        // helper function for the right child
+        if (topmostNode.right != null) {
+            this._leftmostInorder(topmostNode.right);
+        }
+
+        return topmostNode.val;
+    }
+
+    /**
+     * @return whether we have a next smallest number
+     */
+    public boolean hasNext() {
+        return this.stack.size() > 0;
+    }
+}
