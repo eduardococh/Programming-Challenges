4655d331ec315a6480cc99c886ab0c37b81b42c6
Eduardo Contreras <eduardoverd_@hotmail.com>
2020-02-21 20:22:11 -0600

    leetcode.com/problems/shortest-word-distance/

---
+++ Arrays/243ShortestWordDistance.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int shortestDistance(String[] words, String word1, String word2) {
+        final int len=words.length;
+        int index1=-1,index2=-1,shortest=Integer.MAX_VALUE;
+        for(int i=0;i<len;i++){
+            if(words[i].equals(word1)){
+                index1=i;
+            }else if(words[i].equals(word2)){
+                index2=i;
+            }
+            if(index1!=-1 && index2!=-1 && Math.abs(index1-index2)<shortest) shortest=Math.abs(index1-index2);
+        }
+        return shortest;
+    }
+}
