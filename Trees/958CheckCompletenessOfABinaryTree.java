2dd80d2844309ce39c6fe1ccc63dfd2b044a543a
Eduardo Contreras <eduardoverd_@hotmail.com>
2020-03-12 20:55:34 -0600

    leetcode.com/problems/check-completeness-of-a-binary-tree/description/

---
+++ Trees/958CheckCompletenessOfABinaryTree.java
@@ -0,0 +1,15 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isCompleteTree(TreeNode root) {
+        int height=getHeight(root);
+    	return checkTotal(root,height-1);
+    }
+}
