3a96c9491b4c4762c415200741999dc08300c9e0
Eduardo Contreras <eduardoverd_@hotmail.com>
2020-03-17 23:29:07 -0600

    leetcode.com/problems/check-completeness-of-a-binary-tree/

--- Trees/958CheckCompletenessOfABinaryTree.java
+++ Trees/958CheckCompletenessOfABinaryTree.java
@@ -12,4 +12,9 @@ class Solution {
         int height=getHeight(root);
     	return checkTotal(root,height-1);
     }
+        
+    public int checkTotal(TreeNode node, int height){
+        if(checkTotal(node.left,height-1)=
+    }
+
 }
