f693bd1e8728f732cc06f19365152dfa1a90a14c
Eduardo Contreras <eduardoverd_@hotmail.com>
2020-03-29 18:34:43 -0600

    leetcode.com/problems/next-greater-element-i

--- Arrays/496NextGreaterElementI.java
+++ Arrays/496NextGreaterElementI.java
@@ -1,3 +1,8 @@
+       /*
+        *   My solution using hashmap (better brute force as defined by leetcode)
+        *   Amazing runtime better than 99.17% O(M*N) where M is num1 and N num2 
+        *   Bad runtime better than only 7.41% O(N) 
+        */
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         final int len2=nums2.length,len1=nums1.length;
@@ -21,5 +26,25 @@ class Solution {
             }
         }
         return result;
+    }
+}
+
+        //Stack solution by leetcode
+public class Solution {
+    public int[] nextGreaterElement(int[] findNums, int[] nums) {
+        Stack < Integer > stack = new Stack < > ();
+        HashMap < Integer, Integer > map = new HashMap < > ();
+        int[] res = new int[findNums.length];
+        for (int i = 0; i < nums.length; i++) {
+            while (!stack.empty() && nums[i] > stack.peek())
+                map.put(stack.pop(), nums[i]);
+            stack.push(nums[i]);
+        }
+        while (!stack.empty())
+            map.put(stack.pop(), -1);
+        for (int i = 0; i < findNums.length; i++) {
+            res[i] = map.get(findNums[i]);
+        }
+        return res;
     }
 }
