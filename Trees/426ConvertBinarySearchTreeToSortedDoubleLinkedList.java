a556e7a1ab9f6ea52d9fcc849bb00709ce486391
Eduardo Contreras <eduardoverd_@hotmail.com>
2020-04-21 10:46:20 -0500

    leetcode.com/problems/convert-binary-search-tree-to-sorted-doubly-linked-list

--- Trees/426ConvertBinarySearchTreeToSortedDoubleLinkedList.java
+++ Trees/426ConvertBinarySearchTreeToSortedDoubleLinkedList.java
@@ -1,24 +1,10 @@
 /*
-// Definition for a Node.
-class Node {
-    public int val;
-    public Node left;
-    public Node right;
-
-    public Node() {}
-
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val,Node _left,Node _right) {
-        val = _val;
-        left = _left;
-        right = _right;
-    }
-};
+    My initial solution using recursion and returning max and min
+    Amazing runtime of 0ms better than 100% O(N)
+    Bad memory better than only 6.90% O(N)
+    Good solution that I clearly understand, leetcode offers a shorter one
+    and better one I think
 */
-
 class Solution {
     public Node treeToDoublyList(Node root) {
         if(root==null) return null;
@@ -57,4 +43,44 @@ class Solution {
         }
         return result;
     }
+}
+
+    //Recursive solution by leetcode
+    //Same runtime and memory as my solution
+    //Just simple and elegant, depending on what you think of using global variables
+class Solution {
+  // the smallest (first) and the largest (last) nodes
+  Node first = null;
+  Node last = null;
+
+  public Node treeToDoublyList(Node root) {
+    if (root == null) return null;
+
+    helper(root);
+    // close DLL
+    last.right = first;
+    first.left = last;
+    return first;
+  }
+
+  public void helper(Node node) {
+    if (node != null) {
+      // left
+      helper(node.left);
+      // node 
+      if (last != null) {
+        // link the previous node (last)
+        // with the current one (node)
+        last.right = node;
+        node.left = last;
+      } else {
+        // keep the smallest node
+        // to close DLL later on
+        first = node;
+      }
+      last = node;
+      // right
+      helper(node.right);
+    }
+  }
 }
