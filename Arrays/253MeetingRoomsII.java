b8addfde83e39f8a92a8e85226511fec5b287795
Eduardo Contreras <eduardoverd_@hotmail.com>
2020-04-21 22:59:31 -0500

    leetcode.com/problems/meeting-rooms-ii

--- Arrays/253MeetingRoomsII.java
+++ Arrays/253MeetingRoomsII.java
@@ -2,27 +2,24 @@
         //Bad runtime at 10ms better than only 16.80% O(N Log N) because of sorting and
         //because at the worst case we will do N add O(1) and N removals (Log N)
         //Terrible memeory better than only 5% at O(N)
-        //
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
+        ///SAVE METHOD
         //Arrays.sort(intervals, (a, b) -> (if(a[0]==b[0]){Double.compare(a[0], b[0])}));
-        //sort by two columns
-        int result=0;
-        Arrays.sort(intervals, java.util.Comparator.<int[]>comparingInt(a -> a[0]).thenComparingInt(a -> a[1]));
-        PriorityQueue<Integer> meetingRooms=new PriorityQueue<Integer>();
-        for(int[] meeting:intervals){
-            if(meetingRooms.isEmpty()){
-                meetingRooms.add(meeting[1]);
-            }else{
-                while(!meetingRooms.isEmpty() && meetingRooms.peek()<=meeting[0]){//remove all meetings until my start   
-                    meetingRooms.poll();
-                }
-                meetingRooms.add(meeting[1]);
+        Arrays.sort(intervals, Comparator.<int[]>comparingInt(o -> o[0]).thenComparingInt(o -> o[1]));
+        //By default min heap
+        PriorityQueue<Integer> pq=new PriorityQueue<>();
+        int max=0;
+        for(int[] interval:intervals){
+            while(!pq.isEmpty() && pq.peek()<=interval[0]){
+                pq.poll();
             }
-            
-            if(meetingRooms.size()>result) result = meetingRooms.size();
+            pq.add(interval[1]);
+            if(pq.size()>max){
+                max=pq.size();
+            }
         }
-        return result;
+        return max;
     }
 }
 
@@ -30,6 +27,8 @@ class Solution {
         //Super simple solution using two sorted arrays
         //trick is we separate start and end times, making meetings no sense
         //if the meeting at the end index is not over add one to result
+        //This is not a solution someone would get at an interview I think, since sorting the arrays
+        //start and end breaks the relation and it's not very logical
 class Solution {
     public int minMeetingRooms(int[][] intervals) {
         int[] begins = new int[intervals.length];
