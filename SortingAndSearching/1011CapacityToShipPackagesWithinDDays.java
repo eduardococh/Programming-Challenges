38f6015d0c366941c732e798fc09044b1d48185e
Eduardo Contreras <eduardoverd_@hotmail.com>
2020-04-24 23:57:01 -0500

    leetcode.com/problems/capacity-to-ship-packages-within-d-days

--- SortingAndSearching/1011CapacityToShipPackagesWithinDDays.java
+++ SortingAndSearching/1011CapacityToShipPackagesWithinDDays.java
@@ -1,3 +1,7 @@
+        //My linear solution
+        //Terrible runtime of 309ms O(W*T) where W is the number of packages and T the difference
+        //between the biggest package lenght and the total sum of all the packages
+
 class Solution {
     public int shipWithinDays(int[] weights, int D) {
         int res=weights.length;
@@ -24,6 +28,51 @@ class Solution {
             }else{
                 if(days>D) return Integer.MAX_VALUE;
                 days++;
+                weightSoFar=w[currentPackage];
+            }
+            currentPackage++;
+        }
+        return days;
+    }
+
+}
+
+class Solution {
+    public int shipWithinDays(int[] weights, int D) {
+        int res=weights.length;
+        int total=0,max=0;
+        for(int n:weights){
+            if(n>max) max=n;
+            total+=n;
+        }	
+        int low=max;
+        int high=total;
+        while(low<high){//binary searching for the lowest
+            int mid=high-(high-low)/2;
+            int nDays=getDays(mid,weights,D);
+            if(nDays<=D){//right amount of days, could we go lower?
+                int test=getDays(mid-1,weights,D);
+                if(mid>max && getDays(mid-1,weights,D)==Integer.MAX_VALUE){//we're the lowest value possible withing D days
+                    return mid;
+                }else{
+                    high=mid-1;
+                }
+            }else{//taking too long, increase weight capacity
+                low=mid+1;
+            }
+        }
+        return res;
+    }
+    private int getDays(int weight,int w[],int D){
+		int days=1;
+		int currentPackage=0;
+		int weightSoFar=0;
+		while(currentPackage<w.length){
+            if(weightSoFar+w[currentPackage]<=weight){
+                weightSoFar+=w[currentPackage];
+            }else{
+                days++;
+                if(days>D) return Integer.MAX_VALUE;
                 weightSoFar=w[currentPackage];
             }
             currentPackage++;
