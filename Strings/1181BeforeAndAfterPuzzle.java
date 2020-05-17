43e4e3845ef362072c32d54547345c8b3ac94a21
Eduardo Contreras <eduardoverd_@hotmail.com>
2020-05-16 23:54:29 -0500

    leetcode.com/problems/before-and-after-puzzle

--- Strings/1181BeforeAndAfterPuzzle.java
+++ Strings/1181BeforeAndAfterPuzzle.java
@@ -1,3 +1,7 @@
+        //My solution using a hashmap of mappings
+        //Almost all solutions I saw use this two passes and map approach
+        //Map each starting word and their phrases, then loop ending words and
+        //search for matches, avoid same indexes and repeated mergings
         //Runtime of 6ms better than 77.36% O(N)
         //Amazing memory better than 100% O(N)
 class Solution {
@@ -58,5 +62,43 @@ class Solution {
         }
         Collections.sort(result);
         return result;
+    }
+}
+
+        //Sample from leetcode's 3ms
+        //My same approach but optimized
+        //Put words into a map and then iterate
+        //When I ran it I got the same runtime and memory as my solution
+class Solution {
+    public List<String> beforeAndAfterPuzzles(String[] phrases) {
+        String[] newPhrases = new String[phrases.length];
+        Map<String, Set<Integer>> map = new HashMap<>();
+        for(int j = 0; j < phrases.length; j++) {
+            int i = phrases[j].indexOf(' ');
+            String word = "";
+            if(i == -1) word = phrases[j];
+            else word = phrases[j].substring(0, i);
+            newPhrases[j] = i == -1 ? "" : phrases[j].substring(i);
+            
+            map.putIfAbsent(word, new HashSet<>());
+            map.get(word).add(j);
+        }
+        Set<String> resSet = new HashSet<>();
+        for(int k = 0; k < phrases.length; k++) {
+            int i = phrases[k].lastIndexOf(' ');
+            String word = phrases[k].substring(i+1);
+            if(map.containsKey(word)) {
+                for(Integer j : map.get(word)) {
+                    if(k == j) continue;
+                    resSet.add(phrases[k] + newPhrases[j]);
+                }
+            }
+        }
+        
+        List<String> res = new ArrayList<>(resSet);
+        Collections.sort(res);
+        
+        return res;
+        
     }
 }
