5d55adfba8fdb37d1366389b7fbd8e6d18534498
Eduardo Contreras <eduardoverd_@hotmail.com>
2020-06-28 23:49:14 -0500

    leetcode.com/problems/exclusive-time-of-functions

--- StacksQueues/636ExclusiveTimeOfFunctions.java
+++ StacksQueues/636ExclusiveTimeOfFunctions.java
@@ -9,13 +9,16 @@ class Solution {
     public int[] exclusiveTime(int n, List<String> logs) {
         int res[]=new int[n];
         Stack<State> processorWait=new Stack<State>();
-        HashSet<Integer> startedProcesses=new HashSet<Integer>();
-        HashSet<Integer> endedProcesses=new HashSet<Integer>();
+        HashMap<Integer,Integer> startedProcesses=new HashMap<>();
         final int len=logs.size();
         for(int i=0;i<len;i++){
             String currentLog=logs.get(i);
             if(getAction(currentLog).equals("start")){
-                if(startedProcesses.contains(getId(currentLog))) continue;
+                if(startedProcesses.containsKey(getId(currentLog)){
+                    
+                }else{
+                    startedProcesses.put(getId(currentLog),1);
+                }
             }else{
                 if(endedProcesses.contains(getId(currentLog))) continue;
             }
@@ -27,7 +30,6 @@ class Solution {
                     if(!processorWait.isEmpty()){//i will pass my total time as timeoff to father
                         processorWait.peek().timeOff+=totalTime;
                     }
-                    endedProcesses.add(getId(currentLog));
                 }else{
                     startedProcesses.add(getId(currentLog));
                     State procStart=new State();
@@ -65,17 +67,28 @@ class Solution {
     }
 }
 
-
+        //Simplified approach
+class Solution {
     
-  /*  private int processLogs(int id,int index,int[] res,List<String> logs){
-        int globalTime=0;
-        String currentLog=logs.get(id);
-        String[] data=currentLog.split(":");
-        int startTime=Integer.parseInt(data[2]);
-        int logIterator=index+1;
-        while(getIndex(logs.get(logIterator))!=index){
-            int nextId=getId(logs.get(logIterator));
-            processLogs(nextId,logIterator,res,logs);
-        }
+    public int[] exclusiveTime(int n, List<String> logs) {
+        int res[]=new int[n];
+        
+        return res;
     }
-    */
+    
+    private int getId(String log){
+        String[] data=log.split(":");
+        return Integer.parseInt(data[0]);
+    }
+    
+    private String getAction(String log){
+        String[] data=log.split(":");
+        return data[1];
+    }
+    
+    private int getTimestamp(String log){
+        String[] data=log.split(":");
+        return Integer.parseInt(data[2]);
+    }
+
+}
