c840c5ab4325291910d9ae66d3c40e71f3f99eaa
Eduardo Contreras <eduardoverd_@hotmail.com>
2020-07-06 23:33:33 -0500

    leetcode.com/problems/most-common-word

--- Strings/819MostCommonWord.java
+++ Strings/819MostCommonWord.java
@@ -1,3 +1,7 @@
+        //My solution
+        //Good runtime better than 79.6% O(N+B) where N is paragraph lenght and B banned lenght
+        //Bad memory better than only 17.15% O(N+B)
+        //Simple and clear enough
 class Solution {
     public String mostCommonWord(String paragraph, String[] banned) {
         String cleaned=cleaner(paragraph);
@@ -13,13 +17,13 @@ class Solution {
             if(!bannedWords.contains(word)){
                 //I'm not banned
                 if(counter.containsKey(word)){
-                    counter.put(word,couter.get(word)+1); 
-                    if(counter.get(word)>mostOcurrences){
-                        mostOcurrences=counter.get(word);
-                        res=word;
-                    }
+                    counter.put(word,counter.get(word)+1); 
                 }else{
                     counter.put(word,1);
+                }
+                if(counter.get(word)>mostOcurrences){
+                    mostOcurrences=counter.get(word);
+                    res=word;
                 }
             }
         }
@@ -27,6 +31,58 @@ class Solution {
     }
     
     private String cleaner(String paragraph){
-        
+        boolean space=true;
+        paragraph=paragraph.toLowerCase();
+        char[] charArr=paragraph.toCharArray();
+        StringBuilder result=new StringBuilder("");
+        for(char character:charArr){
+            if(character>='a' && character<='z'){
+                result.append(character);
+                space=true;
+            }else if(space){
+                result.append(' ');
+                space=false;
+            }
+        }
+        return result.toString();
+    }
+}
+
+    
+    //Solution from leetcode 4ms samples
+    //Create words as you go, clever approach and avoids creation of cleaner method like me
+    //Runtime of 4ms
+    //NOTE initial point added to paragraph to make it work
+    //When I ran this solution it achieved a worse runtime of 13ms and 16 better than 49% and similar memory
+    //to my solution, probably that my method is probably faster in runtime
+class Solution {
+    public String mostCommonWord(String paragraph, String[] banned) {
+        paragraph += ".";
+
+        Set<String> banset = new HashSet();
+        for (String word: banned) banset.add(word);
+        Map<String, Integer> count = new HashMap();
+
+        String ans = "";
+        int ansfreq = 0;
+
+        StringBuilder word = new StringBuilder();
+        for (char c: paragraph.toCharArray()) {
+            if (Character.isLetter(c)) {
+                word.append(Character.toLowerCase(c));
+            } else if (word.length() > 0) {
+                String finalword = word.toString();
+                if (!banset.contains(finalword)) {
+                    count.put(finalword, count.getOrDefault(finalword, 0) + 1);
+                    if (count.get(finalword) > ansfreq) {
+                        ans = finalword;
+                        ansfreq = count.get(finalword);
+                    }
+                }
+                word = new StringBuilder();
+            }
+        }
+
+        return ans;
     }
 }
