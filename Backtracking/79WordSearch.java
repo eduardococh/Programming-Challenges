07c32e47a557f5d401582d971c328148cc72e524
Eduardo Contreras <eduardoverd_@hotmail.com>
2021-10-06 23:46:48 -0500

    79 Word Search

--- Backtracking/79WordSearch.java
+++ Backtracking/79WordSearch.java
@@ -36,6 +36,35 @@ class Solution {
     }
 }
 
+//Same approach as above, some time later,
+//But this got a terrible runtime of 87ms, despite the same time complexity
+//Not sure why
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        for(int row=0;row<board.length;row++){
+            for(int column=0;column<board[0].length;column++){
+                if(exist(board,word,row,column,0)) return true;
+            }
+        }
+        return false;
+    }
+    
+    public boolean exist(char[][] board, String word,int row,int column,int index){
+        if(row<0 || column<0 || row>=board.length || column>=board[0].length) return false;
+        if(board[row][column]=='0') return false;//already visited
+        if(board[row][column]!=word.charAt(index)) return false;//not a match
+        if(index==word.length()-1) return true;
+        //current position is valid
+        char aux=board[row][column];
+        board[row][column]='0';
+        if(exist(board,word,row+1,column,index+1)) return true;
+        if(exist(board,word,row-1,column,index+1)) return true;
+        if(exist(board,word,row,column+1,index+1)) return true;
+        if(exist(board,word,row,column-1,index+1)) return true;
+        board[row][column]=aux;
+        return false;
+    }
+}
 
 		//Best solution with 2ms, same approach as me
 		//Setting board[x][x]^=256 is an XOR with 256, char get at most 255 so this is 
