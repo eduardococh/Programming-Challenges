8597d2b72fbe003fa6ff2980a0d97908ecbcd9bc
Eduardo Contreras <eduardoverd_@hotmail.com>
2021-10-21 23:57:28 -0500

    leetcode.com/problems/sort-characters-by-frequency

---
+++ Strings/451SortCharactersByFrequency.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String frequencySort(String s) {
+        int count[]=new int[26];
+        for(char car:s.toCharArray()){
+            count[car-'a']++;
+        }
+        
+    }
+}
